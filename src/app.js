ae4b8eaf363b9b228e9e1c0111373f4918f5dbdd
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2022-05-05 19:22:07 +0200

    added get /date method to test jenkins pipeline

--- src/app.js
+++ src/app.js
@@ -5,8 +5,22 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-    return res.status(200).json({result: 'Hello world!'});
+    return res
+        .type("application/json")
+        .status(200)
+        .json({result: 'Hello world!'});
 });
+app.get("/date", (req, res) => {
+    const today = new Date();
+    const expectedResponse = {
+        currentDate: today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate(),
+        currentTime: today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+    };
+    return res
+        .type("application/json")
+        .status(200)
+        .json({result: expectedResponse});
+})
 
 
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -13,4 +13,23 @@ describe("Test example", () => {
                 return done();
             });
     });
+
+    test("GET /date", (done) => {
+        const today = new Date();
+        const expectedResponse = {
+            result: {
+                currentDate: today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate(),
+                currentTime: today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+            }
+        }
+
+        request(app).get("/date")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .expect(expectedResponse)
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
+    })
 });
